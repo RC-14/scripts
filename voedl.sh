0e0c980742bd8d0b05254c107dfcb2b782fbc794
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-03-28 21:47:10 +0200

    Add comments to voedl.sh

--- voedl.sh
+++ voedl.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
+# Check if two arguments were passed
 if [ -z ${1} ] || [ -z ${2} ]; then
+	# Print usage and exit if not
 	echo "Usage: voedl.sh <url> <filename>"
 	exit 1
 fi
 
+# Download the html file and search for the encoded mp4 source
 ENCODED=$(curl -s "${1}" | rg -e 'sources\["mp4"\]\s=\s[^\(]+\(([^\)]+)\);' -r '$1')
 
+# Decode the mp4 source with node
 URL=$(node -e "let input = ${ENCODED}; input = input.join('').split('').reverse().join(''); input = atob(input); console.log(input)")
 
+# Download the mp4 video and write it to the specified filename
 curl "${URL}" -o ${2}
+
