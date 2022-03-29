02613fedd72eec81ad38db4621ac376a4609dbb1
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-03-29 15:14:36 +0200

    add source to noUrgency.sh

--- noUrgency.sh
+++ noUrgency.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Source: https://github.com/skontar/Utility/blob/master/scripts/start-apps
 
 for win in $(wmctrl -l | awk -F' ' '{print $1}'); do
     wmctrl -i -r $win -b remove,demands_attention
