32f3c57adb91ea95eaf77b89c707dc64ccf9e863
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-04-02 23:28:46 +0200

    Improve filename handling in voedl.sh

--- voedl.sh
+++ voedl.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
-# Check if two arguments were passed
-if [ -z ${1} ] || [ -z ${2} ]; then
+# Check if at least one argument is given
+if [[ -z "${1}" ]]; then
 	# If not print usage and exit else continue
 	echo "Usage: voedl.sh <url> <filename>"
 	exit 1
 fi
+
+# Check if a second argument is given
+if [[ -z "${2}" ]]; then
+	# If not set FILENAME to default
+	FILENAME="voedl-0.mp4"
+elif [[ "${2}" == *.mp4 ]]; then
+	# If it is a mp4 file set FILENAME to the given filename
+	FILENAME="${2}"
+else
+	# If it is not a mp4 file set FILENAME to the given filename with .mp4 extension
+	FILENAME="${2}.mp4"
+fi
+
+# Check if the file already exists
+if [[ -f ${FILENAME} ]]; then
+	# If it does print a warning and exit if the second argument is set
+	if [[ -n "${2}" ]]; then
+		echo "File ${FILENAME} already exists"
+		exit 1
+	fi
+
+	# If the second argument is not set increment the number in the filename until it is unique
+	while [[ -f ${FILENAME} ]]; do
+		# Remove .mp4 extension because it contains a number
+		FILENAME=${FILENAME%.mp4}
+		# Get the number from the filename
+		FILENAME=${FILENAME//[!0-9]/}
+		# Create a new filename with the incremented number
+		FILENAME="voedl-$((${FILENAME}+1)).mp4"
+	done
+fi
+
+echo "Downloading ${1} to ${FILENAME}"
 
 # Download the html file and use a regex to search for the encoded mp4 source (group 1 is the encoded mp4 source)
 ENCODED=$(curl -s "${1}" | rg -e 'sources\["mp4"\]\s=\s[^\(]+\(([^\)]+)\);' -r '$1')
@@ -14,5 +47,5 @@ ENCODED=$(curl -s "${1}" | rg -e 'sources\["mp4"\]\s=\s[^\(]+\(([^\)]+)\);' -r '
 URL=$(node -e "let input = ${ENCODED}; input = input.join('').split('').reverse().join(''); input = atob(input); console.log(input)")
 
 # Download the mp4 video and write it to the specified filename
-curl "${URL}" -o ${2}
+curl "${URL}" -o "${FILENAME}.mp4"
 
