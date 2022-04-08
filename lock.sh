bf9614fd3cc867058a152fe5ea21c5c7b6350abc
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-04-08 18:14:01 +0200

    remove the .sh extension from all files

--- lock.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# https://archived.forum.manjaro.org/t/solved-changing-blurlock-to-i3lock-after-suspend/75966
-
-i3lock -i ~/Pictures/LockScreen.jpg -e
--- noUrgency.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Source: https://github.com/skontar/Utility/blob/master/scripts/start-apps
-
-for win in $(wmctrl -l | awk -F' ' '{print $1}'); do
-    wmctrl -i -r $win -b remove,demands_attention
-done
